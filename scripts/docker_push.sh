fa8bde671eebc8396ce318b75646344ef19f7513
Matt Lang <langma@gmail.com>
2021-05-19 19:50:01 -0500

    add scripts for build and push

---
+++ scripts/docker_push.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while true; do
+  read -p "Enter version tag, with format YYYY_MM_DDrcVV: " -r
+  case $REPLY in
+    20[0-9][0-9]_[0-9][0-9]_[0-9][0-9]rc[0-9][0-9] ) export RC_VERSION=$REPLY; break;;
+    * ) echo "Enter a correct value...";;
+  esac
+done
+
+echo "Using $RC_VERSION..."
+read -p "Tag and push with $RC_VERSION? " -n 1 -r
+echo 
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Run scripts/docker_push.sh to push when ready..."
+  exit 1
+fi
+
+docker tag jserver:latest gcr.io/rhodes-cs/jserver:$RC_VERSION
+echo
+echo "Pushing..."
+echo
+docker push gcr.io/rhodes-cs/jserver:$RC_VERSION
+echo
+echo "Validate the version appears below:"
+echo
+gcloud container images list-tags gcr.io/rhodes-cs/jserver
+
+
+
+
+
