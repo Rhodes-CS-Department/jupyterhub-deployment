5caa42e8047f43fe3435e3cea7d1cf104be0068c
anesepark <anesepark@gmail.com>
2021-05-24 14:37:22 -0400

    updated helm chart

--- scripts/helm_upgrade.sh
+++ scripts/helm_upgrade.sh
@@ -7,5 +7,5 @@ helm upgrade --cleanup-on-fail \
   --install $RELEASE jupyterhub/jupyterhub \
   --namespace $NAMESPACE \
   --create-namespace \
-  --version=0.9.0 \
+  --version=0.11.1 \
   --values config/config.yaml
