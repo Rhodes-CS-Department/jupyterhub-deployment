1c126f75370d3b1c2d7cb0d7fe44da417e8a6de2
Marion Lang <langma@gmail.com>
2023-01-06 23:49:33 -0600

    update to helm chart 2.0.0

--- scripts/helm_upgrade.sh
+++ scripts/helm_upgrade.sh
@@ -35,7 +35,7 @@ helm upgrade --cleanup-on-fail \
   --install $RELEASE jupyterhub/jupyterhub \
   --namespace $NAMESPACE \
   --create-namespace \
-  --version=1.2.0 \
+  --version=2.0.0 \
   $dry_run \
   --values $secrets \
   --values config/config.yaml 
