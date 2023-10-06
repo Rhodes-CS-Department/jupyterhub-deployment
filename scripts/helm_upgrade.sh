5d732eeffe6cae79b8a4d5df3614a789cfa691a6
Matt Superdock <msuperdock@gmail.com>
2023-10-06 17:29:41 -0500

    upgrade jupyterhub to fix version mismatch

--- scripts/helm_upgrade.sh
+++ scripts/helm_upgrade.sh
@@ -35,7 +35,7 @@ helm upgrade --cleanup-on-fail \
   --install $RELEASE jupyterhub/jupyterhub \
   --namespace $NAMESPACE \
   --create-namespace \
-  --version=2.0.0 \
+  --version=3.1.0 \
   $dry_run \
   --values $secrets \
   --values config/config.yaml 
